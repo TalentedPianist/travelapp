8a27ef99ec4c092655d44887bc3d9eeb451077ad
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-04-07 10:05:03 +0100

    Reusable menu component finally working!!

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View, Keyboard, TouchableWithoutFeedback, TouchableOp
 import Svg, { Path } from 'react-native-svg';
 import styled from 'styled-components';
 import BookingForm from './Components/BookingForm/Form';
-import MyMenu from './Components/Menu';
+import MyMenu from './Components/MyMenu'; // This was set wrong, causing all the grief.
 import { Provider as PaperProvider, Menu } from 'react-native-paper';
 
 const Header = styled.View`
-  display: flex;
+  display: flex; 
   flex-direction: row;
   align-items: center;
   justify-content: space-evenly;
@@ -84,34 +84,24 @@ const DismissKeyboard = ({ children }) => (
 );
 
 export default function App() {
-      const [visible, setVisible] = useState(false);
-      const openMenu = () => setVisible(true);
-      const closeMenu = () => setVisible(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  // Note that the <TouchableOpacity> tag needs to be around the <MyMenu> component in App.js, or it just won't work.  
 
   return (
     <>
-      <DismissKeyboard>
-        <PaperProvider>
+      <PaperProvider>
+        <DismissKeyboard>
           <Header>
+
             <Svg width="90" height="90" viewBox="0 0 58 54" fill="none" xmlns="http://www.w3.org/2000/svg" onPress={() => console.log('Home button was pressed')}>
               <Path d="M42.6667 18.5804L28.9792 4.89292L7.41375 26.4583H12.25V50.7917H21.375V32.5417H36.5833V50.7917H45.7083V26.4583H50.5446L45.7083 21.6221V11.25H42.6667V18.5804ZM0.0833321 29.5L28.9792 0.604167L39.625 11.25V8.20833H48.75V20.375L57.875 29.5H48.75V53.8333H33.5417V35.5833H24.4167V53.8333H9.20833V29.5H0.0833321Z" fill="black" />
             </Svg>
-        
-            <Menu
-                    visible={visible}
-                    onDismiss={closeMenu}
-                    anchor={
-                        <TouchableOpacity onPress={openMenu} style={{ padding: 10 }}>
-                            <Svg width="66" height="44" viewBox="0 0 66 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <Path d="M0 44V36.6667H66V44H0ZM0 25.6667V18.3333H66V25.6667H0ZM0 7.33333V0H66V7.33333H0Z" fill="black" />
-                            </Svg>
-                        </TouchableOpacity>
-                    }>
-                    <Menu.Item onPress={() => { }} title="Item 1" />
-                    <Menu.Item onPress={() => { }} title="Item 2" />
-                </Menu>
-          
+
+            <MyMenu />
           </Header>
+
           <CallToAction>
             <Title>Welcome!</Title>
             <Slug>The Travel App</Slug>
@@ -125,8 +115,9 @@ export default function App() {
           <Footer>
             <FooterText>Copyright &copy; The Travel App 2025</FooterText>
           </Footer>
-        </PaperProvider>
-      </DismissKeyboard>
+
+        </DismissKeyboard>
+      </PaperProvider>
     </>
   );
 }
