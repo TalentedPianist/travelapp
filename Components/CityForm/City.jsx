d672c8449727950c37102a5fa6c0b6a50499fd6b
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-04-13 20:02:17 +0100

    Implemented drop down list for city search!!

--- Components/CityForm/City.jsx
+++ Components/CityForm/City.jsx
@@ -40,36 +40,38 @@ export default function City() {
         }
     }
 
-    const handleSelectItem = (item) => { 
-        if (item?.title === "Glasgow") { 
+    const handleSelectItem = (item) => {
+        if (item?.title === "Glasgow") {
         }
     };
 
-    const handleGetCity = () =>  {
+    const handleGetCity = () => {
         getCity();
 
     }
 
-    const getSuggestions = useCallback(async (q) => { 
+    const getSuggestions = useCallback(async (q) => {
+
         const filterToken = q.toLowerCase();
         console.log(q);
-        if (q.length < 3) { 
+        if (q.length < 3) {
             setSuggestionsList(null);
         }
         setLoading(true);
         axios.get('https://tripadvisor16.p.rapidapi.com/api/v1/hotels/searchLocation', {
-            headers: { 
+            headers: {
                 'x-rapidapi-key': '23da1d3f7amshb4f9b46ad4fbdf7p1528f2jsn716390f369ba',
                 'x-rapidapi-host': 'tripadvisor16.p.rapidapi.com',
-            }, 
-            params: { query: 'Glasgow' },
+            },
+            params: { query: q },
         }).then((res) => {
-           const formattedSuggestions = res.data.data.map((item, index) => ({ 
-            id: index.toString(),
-            title: item.name, // Remove HTML tags
-           }));
-           setSuggestionsList(res.data.data);
-           console.log(res);
+            let title = undefined;
+            const formattedSuggestions = res.data.data.map((item, index) => ({
+                id: index.toString(),
+                title: `${item.title}, ${item.secondaryText}`,
+            }));
+            setSuggestionsList(formattedSuggestions);
+            console.log(formattedSuggestions);
         }).then((err) => console.error(err));
     }, []);
 
@@ -77,8 +79,8 @@ export default function City() {
         console.log(suggestionsList);
     }, []);
 
-    
-    
+
+
     return (
         <>
             <View style={styles.cityContainer}>
@@ -93,15 +95,38 @@ export default function City() {
                         ref={searchRef}
                         onChangeText={getSuggestions}
                         dataSet={suggestionsList}
+                        style={styles.autoDropdown}
+                        textInputProps={{
+                            placeholder: "Type a city name...",
+                            placeholderTextColor: '#000',
+                            autoCorrect: false,
+                            autoCapitalize: 'none',
+                            style: {
+                                backgroundColor: 'orange',
+                                width: '100%',
+                            }
+                        }}
+                        renderItem={(item, text) => <Text style={{
+                            backgroundColor: 'yellow',
+                            color: '#000',
+                            fontSize: 22,
+                            width: '100%',
+                            paddingLeft: 20,
+                            paddingRight: 20,
+                            paddingTop: 20,
+                            paddingBottom: 20,
+                        }}>{item.title.replace(/<([^>]+)>/gi, '')
+                            }
+                        </Text>}
                     />
 
-                    
+
 
 
                     <TouchableOpacity onPress={handleGetCity} style={styles.buttonText}>
                         <Text style={styles.buttonText}>Search</Text>
                     </TouchableOpacity >
-                   
+
                 </PaperProvider >
             </View >
         </>
@@ -115,6 +140,8 @@ const styles = StyleSheet.create({
         paddingTop: 20,
         paddingBottom: 20,
         display: 'flex',
+        width: '100%',
+        height: '100%',
     },
     cityText: {
         color: 'white',
@@ -128,17 +155,19 @@ const styles = StyleSheet.create({
     buttonText: {
         backgroundColor: 'yellow',
         color: 'black',
-        paddingTop: 5,
         paddingLeft: 5,
         paddingRight: 5,
-        paddingBottom: 5,
+        fontSize: 22,
         display: 'flex',
         alignSelf: 'flex-start',
-        marginTop: 5,
+        marginTop: 20,
         marginBottom: 20,
     },
     headerStyle: {
         fontSize: 26,
         paddingBottom: 10,
+    },
+    autoDropdown: {
+        width: '100%',
     }
 });
